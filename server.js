c1f6e52340fdfdf6164d25db5961fe0e38ba79bd
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2015-02-22 19:53:55 -0300

    Added socket.io interaction. Added UNIX server to server.js.

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
+var net = require('net');
 var express = require('express')
-var app = express()
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var unixServer = net.createServer(function(client) {
+    console.log('Client connected');
+    client.on('data', function(data){
+      console.log(data.toString());
+    })
+});
+
+unixServer.listen('/var/run/inscripcion-alert-socket');
+
+io.on('connection', function(socket){
+  console.log('Connected');
+})
 
 // Serve public on /
 app.use('/', express.static('public'));
 
 // Listen
-var server = app.listen(80, function () {
+server.listen(80, function () {
 
   var host = server.address().address
   var port = server.address().port
